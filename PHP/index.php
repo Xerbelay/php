54c3b4c803fe3cc1f6eb37e2ed2e7f87ba30dd2a
Xerbelay <148629656+Xerbelay@users.noreply.github.com>
2024-04-13 13:26:23 +0300

    Add files via upload

---
+++ PHP/index.php
@@ -0,0 +1,90 @@
+<?php
+    error_reporting(E_ALL);
+    ini_set('display_errors', 'on');
+
+    /* задание 1 */
+/*     $a = 5;
+    $b = 15;
+    if($a > $b){
+        echo $a;
+    }
+    else{
+        echo $b;
+    }; */
+
+    /* задание 2 */
+/*     $a = 155;
+    $b = 255;
+    if($a - $b == 100 || $b - $a == 100){
+        echo 'да';
+    }
+    else{
+        echo 'нет';
+    } */
+
+    /* задание 3 */
+/*     $a = 100;
+    $b = 120;
+    if(($a - $b <= 20 and $a - $b >= 0) xor ($b - $a <= 20 and $b - $a >= 0)){
+        echo 'да';
+    }
+    else{
+        echo 'нет';
+    } */
+
+    /* задание 4 */
+/*     $months = 10;
+    switch($months) {
+        case 1: case 2: case 12: echo 'зима';
+        break;
+        case 3: case 4: case 5: echo 'весна';
+        break;
+        case 6: case 7: case 8: echo 'лето';
+        break;
+        case 9: case 10: case 11: echo 'осень';
+        break;
+        default : echo "Oшибка"; break;
+    } */
+
+    /* задание 5 */
+/*     $role = 'admin';
+    $isAdmin = '';
+    if($role == 'admin'){
+        $isAdmin = true;
+    }
+    else{
+        $isAdmin = false;
+    }
+    switch($isAdmin) {
+        case true: ?> <div class='flex'><h1>Привет админ!</h1><button class='button'>Войти в панель управления</button></div><?;
+        break;
+        case false: ?> <span class='bold'>У вас нет прав доступа</span> <?;
+        break;
+        default : echo "Ошибка"; break;
+    } */
+
+    /* задание 6 */
+/*     $lang = 'de';
+    switch($lang) {
+        case 'ru': echo 'Привет';
+        break;
+        case 'en': echo 'hi';
+        break;
+        case 'de': echo 'Hallo';
+        break;
+        default : echo "Данный язык отсутсвует"; break;
+    } */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
